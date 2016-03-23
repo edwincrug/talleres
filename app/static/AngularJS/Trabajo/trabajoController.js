387c7fefad3029d5b4d8dfaae72e726a0afce8a7
mmejiar <mario_mejram@hotmail.com>
2016-03-23 14:30:28 -0600

    creación de controllers y repository MAMR

---
+++ app/static/AngularJS/Trabajo/trabajoController.js
@@ -0,0 +1,12 @@
+// -- =============================================
+// -- Author:      Mario Mejía
+// -- Create date: 23/03/2016
+// -- Description: trabajo controller
+// -- Modificó: 
+// -- Fecha: 
+// -- =============================================
+registrationModule.controller('trabajoController', function($scope, alertFactory){
+	//this is the first method executed in the view
+	$scope.init = function(){
+	}
+});
