e19f0ca73f917aaccfeb15b33fcbdcd5f37e52ba
urielgangster <porkpie_mod@hotmail.com>
2016-03-28 10:43:33 -0600

    Controllers, Repositorys
    Primera versión de controllers y repositorys

---
+++ app/static/AngularJS/Citas/citasController.js
@@ -0,0 +1,11 @@
+// -- =============================================
+// -- Author:      Uriel Godínez Martínez
+// -- Create date: 23/03/2016
+// -- Description: Citas controller
+// -- Modificó: 
+// -- Fecha: 
+// -- =============================================
+registrationModule.controller('citasController', function($scope, alertFactory){
+	$scope.init = function(){
+	}
+});
---
+++ app/static/AngularJS/CotizacionDetalle/cotizacionDetalleController.js
@@ -0,0 +1,11 @@
+// -- =============================================
+// -- Author:      Uriel Godínez Martínez
+// -- Create date: 23/03/2016
+// -- Description: Cotización Detalle controller
+// -- Modificó: 
+// -- Fecha: 
+// -- =============================================
+registrationModule.controller('cotizacionDetalleController', function($scope, alertFactory){
+	$scope.init = function(){
+	}
+});
