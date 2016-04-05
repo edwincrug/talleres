08edef2beca02207d967af4fd586bad97ad75d57
mmejiar <mario_mejram@hotmail.com>
2016-04-04 18:26:16 -0600

    se elimina código

--- app/static/AngularJS/Cotizacion/cotizacionRepository.js
+++ app/static/AngularJS/Cotizacion/cotizacionRepository.js
@@ -3,14 +3,13 @@ var searchUrl = global_settings.urlCORS + '/api/cotizacion/';
 registrationModule.factory('cotizacionRepository', function ($http) {
     return {
         get: function () {
-            // return $http.post(loginUrl + '1|' + rol + '|' + nombre + '|' + email + '|' + password);
             return $http({
                 url: searchUrl + 'see',
                 method: "GET"
             });
         },
         busquedaPieza: function(){
-        	return $http:({
+        	return $http({
         		url: searchUrl + 'buscar',
         		method: "GET"
         	});
