4abbcbdb88f79ff9d86b36b672135fb340ca39a0
mmejiar <mario_mejram@hotmail.com>
2016-04-18 10:53:14 -0600

    cambio cotizacion

--- app/expressServer.js
+++ app/expressServer.js
@@ -13,7 +13,7 @@ var storage = multer.diskStorage({
     var idCotizacion = req.body.idCotizacion;
     mkdirp(__dirname + '/static/uploads/files/' + idTrabajo, function (err) {
          mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion, function (err) {
-           if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif'){
+           if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif' || file.mimetype == 'image/jpg'){
               mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia', function (err) {
                 cb(null, __dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia')
               });
